34a16f448338d22ad42dae10a58a4f0607bf4f60
Maaz Saleem <F2017266085@umt.edu.pk>
2021-02-10 15:49:53 +0500

    intital

---
+++ app/src/main/java/com/example/firenotes/AddNote.java
@@ -0,0 +1,77 @@
+package com.example.firenotes;
+
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddNote extends AppCompatActivity {
+    FirebaseFirestore fstore;
+    EditText noteTitle,noteContent;
+    ProgressBar progressBarSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fstore= FirebaseFirestore.getInstance();
+        noteContent=findViewById(R.id.addNoteContent);
+        noteTitle=findViewById(R.id.addNoteTitle);
+
+        progressBarSave=findViewById(R.id.progressBar);
+
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nTitle=noteTitle.getText().toString();
+                String nContent=noteContent.getText().toString();
+                if(nTitle.isEmpty()|| nContent.isEmpty()){
+                    Toast.makeText(AddNote.this,"Can not Save note with Empty Field",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBarSave.setVisibility(View.VISIBLE);
+
+                DocumentReference docref=fstore.collection("notes").document();
+                Map<String,Object> note=new HashMap<>();
+                note.put("title",nTitle);
+                note.put("content",nContent);
+
+                docref.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(AddNote.this, "note added", Toast.LENGTH_SHORT).show();
+                        onBackPressed();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddNote.this, "Error try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firenotes/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.firenotes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.firenotes.model.Adapter;
+import com.example.firenotes.model.Note;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView nav_view;
+    RecyclerView noteLists;
+    Adapter adapter;
+    FirebaseFirestore fstore;
+    FirestoreRecyclerAdapter<Note,NoteViewHolder> noteadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        fstore=FirebaseFirestore.getInstance();
+        Query query = fstore.collection("notes").orderBy("title", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> allNotes=new  FirestoreRecyclerOptions.Builder< Note >()
+                .setQuery(query, Note.class)
+                .build();
+            noteadapter=new FirestoreRecyclerAdapter<Note, NoteViewHolder>(allNotes) {
+                @Override
+                protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int i, @NonNull Note note) {
+                    noteViewHolder.noteTitle.setText(note.getTitle());
+                    noteViewHolder.noteContent.setText(note.getContent());
+                    Integer code=getRandomColor();
+                    noteViewHolder.mCardView.setCardBackgroundColor(noteViewHolder.view.getResources().getColor(code,null));
+                    String docId=noteadapter.getSnapshots().getSnapshot(i).getId();
+                    noteViewHolder.view.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i=new Intent(v.getContext(), NoteDetails.class);
+                            i.putExtra("title",note.getTitle());
+                            i.putExtra("content",note.getContent());
+                            i.putExtra("name",code);
+                            i.putExtra("noteId",docId);
+                            v.getContext().startActivity(i);
+                        }
+                    });
+                }
+
+                @NonNull
+                @Override
+                public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view_layout,parent,false);
+                    return new NoteViewHolder(view);
+                }
+            };
+
+        noteLists=findViewById(R.id.notelist);
+        drawerLayout=findViewById(R.id.drawer);
+        nav_view=findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+
+
+
+        noteLists.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        noteLists.setAdapter(noteadapter);
+        FloatingActionButton fab=findViewById(R.id.addNoteFloat);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(),AddNote.class));
+            }
+        });
+
+        }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.addNote:
+                startActivity(new Intent(this,AddNote.class));
+
+            default:
+                Toast.makeText(this,"soon",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            System.exit(0);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView noteTitle,noteContent;
+        View view;
+        CardView mCardView;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle=itemView.findViewById(R.id.titles);
+            noteContent=itemView.findViewById(R.id.content);
+            mCardView=itemView.findViewById(R.id.noteCard);
+            view=itemView;
+        }
+
+
+
+    }
+    private int getRandomColor() {
+        List<Integer> colorCode=new ArrayList<>();
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.lightPurple);
+        colorCode.add(R.color.lightGreen);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.greenlight);
+        colorCode.add(R.color.notgreen);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.red);
+        Random randomColor=new Random();
+        int number= randomColor.nextInt(colorCode.size());
+        return  colorCode.get(number);
+
+    }
+    protected void onStart(){
+        super.onStart();
+        noteadapter.startListening();
+    }
+    protected void onStop() {
+        super.onStop();
+        if(noteadapter!=null){
+            noteadapter.stopListening();
+        }
+    }
+
+
+
+    }
